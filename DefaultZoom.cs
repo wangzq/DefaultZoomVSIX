1bcbdde2b11dcc3fda11242971b4b2fee34d530a
wawang <wawang@microsoft.com>
2015-05-15 07:18:17 +0800

    Initial commit.

---
+++ DefaultZoom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace DefaultZoomVSIX
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("code")]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal class DefaultZoom : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            textView.GotAggregateFocus += TextView_GotAggregateFocus;
+        }
+
+        private void TextView_GotAggregateFocus(object sender, EventArgs e)
+        {
+            var textView = (IWpfTextView)sender;
+            textView.ZoomLevel = 99.0;
+        }
+    }
+}
